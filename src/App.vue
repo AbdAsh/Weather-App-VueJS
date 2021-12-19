554af60b64999d0c1c657e06e43a44a095e43533
AbdAsh <59181667+AbdAsh@users.noreply.github.com>
2021-12-19 03:46:57 +0300

    Hourly Forecast Details Tab fully Implemented

--- src/App.vue
+++ src/App.vue
@@ -16,4 +16,7 @@ body {
   padding: 0;
   margin: 0;
 }
+img {
+  user-select: none;
+}
 </style>
--- src/components/HourlyForcast.vue
+++ src/components/HourlyForcast.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="container">
-    <div class="arrow-container" @click.prevent="prevDay">
+    <div
+      class="arrow-container"
+      :class="selected < 1 ? 'disabled' : ''"
+      @click.prevent="prevHour"
+    >
       <img class="flipped" src="../assets/next.png" alt="Left" />
     </div>
     <div class="hourly-forecast">
@@ -9,7 +13,7 @@
         :key="i"
         class="hour-container"
         :class="i == selected ? 'selected' : ''"
-        @click="selectDay(i)"
+        @click="selectHour(i)"
       >
         <div class="hour">
           <p>
@@ -30,7 +34,7 @@
               src="../assets/water.png"
               alt="Weather Icon"
             />
-            <p>{{ result.humidity }}%</p>
+            <p>{{ result.pop * 100 }}%</p>
             <div
               class="wind"
               :style="{ transform: `rotate(${result.wind_deg + 45}deg)` }"
@@ -43,10 +47,47 @@
             </div>
           </div>
         </div>
-        <div v-if="i == selected" class="details"></div>
+        <div v-if="i == selected" class="details">
+          <p class="main">{{ result.weather[0].description }}</p>
+          <div>
+            <p>
+              Humidity: <b>{{ result.humidity }}%</b>
+            </p>
+            <p>
+              Pressure: <b>{{ result.pressure }}mb</b>
+            </p>
+            <p>
+              Visibility:
+              <b>{{ result.visibility > 500 ? "Clear" : "Foggy" }}</b>
+            </p>
+            <div class="spacer"></div>
+            <p>
+              Temprature feels like <b>{{ result.feels_like }}&deg;</b>
+            </p>
+            <div class="spacer"></div>
+            <p>
+              {{
+                result.pop > 50
+                  ? "High chance of precipitation"
+                  : "Low chance of precipitation"
+              }}
+            </p>
+            <div class="spacer"></div>
+            <p>
+              A
+              {{ result.wind_speed > 10 ? "strong wind" : "gentle breeze" }}
+              from the
+              {{ result.wind_deg > 180 ? "south" : "north" }}
+            </p>
+          </div>
+        </div>
       </div>
     </div>
-    <div class="arrow-container" @click.prevent="nextDay">
+    <div
+      class="arrow-container"
+      :class="selected < hourlyForecast.length - 1 ? '' : 'disabled'"
+      @click.prevent="nextHour"
+    >
       <img src="../assets/next.png" alt="Right" />
     </div>
   </div>
@@ -66,16 +107,19 @@ export default {
     };
   },
   methods: {
-    nextDay() {
+    nextHour() {
       let limit = this.hourlyForecast.length - 1;
       if (this.selected < limit) {
         this.selected++;
       }
     },
-    prevDay() {
+    prevHour() {
       if (this.selected > 0) {
         this.selected--;
       }
+    },
+    selectHour(hour) {
+      this.selected = hour;
     },
   },
   mounted() {
@@ -100,7 +144,7 @@ export default {
     width: 100%;
     max-width: calc(100% - 4rem);
     height: 100%;
-    background-color: rgb(228, 228, 228);
+    background-color: rgb(233, 235, 225);
     margin: 1px;
     overflow-x: scroll;
     overflow-y: hidden;
@@ -113,8 +157,6 @@ export default {
     }
     &::-webkit-scrollbar-thumb {
       background: lightgrey;
-      // box-shadow: 0px 1px 0 2px #6f6f6f, inset 0px 2px 0 1px rgba(255, 255, 255, 0.7), inset 0px -1px 0 1px rgba(140, 140, 140, 0.5), -3px 3px 10px 0 rgba(0, 0, 0, 0.5);
-      border-radius: 10px;
     }
     .hour-container {
       transform: rotateX(180deg);
@@ -126,8 +168,9 @@ export default {
       height: 100%;
       background-color: #fff;
       margin: 0 1px;
+      cursor: pointer;
       &.selected {
-        border-top: 5px solid greenyellow;
+        border-top: 5px solid lightgreen;
       }
       .hour {
         min-width: 5rem;
@@ -163,14 +206,29 @@ export default {
         }
       }
       .details {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: center;
         position: relative;
         min-width: 20rem;
         height: 100%;
         background-color: rgb(226, 225, 225);
+        text-transform: capitalize;
+        text-align: left;
+        .main {
+          font-size: 1.5rem;
+          font-weight: bold;
+          width: 75%;
+        }
       }
     }
   }
   .arrow-container {
+    &.disabled {
+      background-color: rgba(160, 160, 160, 0.432);
+      cursor: default;
+    }
     width: 2rem;
     height: 20%;
     background-color: rgb(92, 92, 92);
@@ -186,6 +244,10 @@ export default {
       height: 1.5rem;
       filter: invert(1);
     }
+  }
+  .spacer {
+    width: 100%;
+    border-bottom: 1px lightgrey solid;
   }
 }
 </style>
